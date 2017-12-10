28dc5047bf45a5de05aa84699cd8576ed6a708bd
Alex <iseldomused@gmail.com>
2017-12-10 05:52:19 +0300

    upd 09

---
+++ 09/04/Circumference.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Circumference : MonoBehaviour {
+
+	void Start () {
+		int radius = 371;
+		double circumference = 2 * 3.14 * radius;
+		print("Длина окружности с радиусом " + radius + " равна " + circumference);
+	}
+}
