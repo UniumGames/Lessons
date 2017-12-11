89c17a3ae6b6ceec80e558d3bcd288b2349fb32a
Alex <iseldomused@gmail.com>
2017-12-12 01:18:57 +0300

    add 10

---
+++ 10/01/MaxHeight.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaxHeight : MonoBehaviour {
+	private float maxHeight = 0;
+	void Update () {
+		// если новая высота больше, чем максимальная
+		if (transform.position.y > maxHeight) {
+			// то новая высота становится максимальной
+			maxHeight = transform.position.y;
+		}
+		print(maxHeight);
+	}
+}
