8f6f762b80f33446a43a4f35bdd08123db43d1aa
Alex <iseldomused@gmail.com>
2017-12-12 02:27:39 +0300

    add 10/03

---
+++ 10/03/TrafficLight.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrafficLight : MonoBehaviour {
+	double time = 0;
+	void Update() {
+		time = time + Time.deltaTime;
+
+		// находим и сохраняем компонент рендерер,
+		// в котором хранится материал
+		Renderer rend = GetComponent<Renderer>();
+
+		// после третьей секунды сбрасываем время на ноль,
+		// чтобы начать заново с зеленого цвета.
+		// в остальное время включаем нужный цвет
+		if (time > 3) {
+			time = 0;
+		}
+		else if (time > 2) {
+			rend.material.color = Color.red;
+		}
+		else if (time > 1) {
+			rend.material.color = Color.yellow;
+		}
+		else if (time > 0) {
+			rend.material.color = Color.green;
+		}
+	}
+}
