28d3ae9f93cd4bb2fba23a57eb051293fe414e2a
Alex <iseldomused@gmail.com>
2017-12-20 19:11:45 +0300

    add 11/01

---
+++ 11/01/ExitButton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExitButton : MonoBehaviour {
+	void Start() {
+		Button button = GetComponent<Button>();
+		button.onClick.AddListener(ExitGame);
+	}
+
+	void ExitGame() {
+		Application.Quit();
+	}
+}
---
+++ 11/01/StartButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartButton : MonoBehaviour {
+	void Start () {
+		Button button = GetComponent<Button>();
+		button.onClick.AddListener(LoadGame);
+	}
+
+	void LoadGame() {
+		SceneManager.LoadScene("Football");
+	}
+}
