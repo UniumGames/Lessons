5a066df0d2e5c3855aa69e199f9966926d4cd8cd
Alex <iseldomused@gmail.com>
2017-12-26 04:43:07 +0300

    upd 11, add tasks

---
+++ 11/02/LightSwitcher.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LightSwitcher : MonoBehaviour {
+	// публичное свойство, чтобы передать ссылку на источник света
+	public Light light;
+	Text textField;
+
+	void Start () {
+		Button switchButton = GetComponent<Button>();
+		textField = GetComponentInChildren<Text>();
+		switchButton.onClick.AddListener(SwitchLight);
+	}
+	
+	void SwitchLight () {
+		// если свет включен
+		if (light.intensity == 1) {
+			// то выключаем
+			light.intensity = 0;
+			// и меняем текст на кнопке
+			textField.text = "Выключено";
+		}
+		// если свет выключен,
+		// то все наоборот
+		else {
+			light.intensity = 1;
+			textField.text = "Включено";
+		}
+	}
+}
---
+++ 11/03/README.md
@@ -0,0 +1,9 @@
+### Задание №3: Дизайн главного меню
+
+Посмотри на три плохих варианта главного меню. Найди в них недостатки, исправь и нарисуй свой вариант главного меню.
+
+![](http://unity3d.unium.ru/lessons/lesson11/images/ui1.jpg)
+
+![](http://unity3d.unium.ru/lessons/lesson11/images/ui2.jpg)
+
+![](http://unity3d.unium.ru/lessons/lesson11/images/ui3.jpg)
